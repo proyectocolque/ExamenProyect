8a58cd81c3da306fce421a9834e778997f8c4a90
proyectocolque <colquecondoricarla28@gmail.com>
2025-09-23 21:04:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'; // ¡Asegúrate de que esta línea esté aquí!
+import Vue from 'vue'; 
 import App from './App.vue';
 import router from './router'; 
 
-// Importa los estilos de Bootstrap
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
