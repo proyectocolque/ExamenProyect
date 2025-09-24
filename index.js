db2dbf3acc19ae0820e8abb75d7f7cbd6487874d
proyectocolque <colquecondoricarla28@gmail.com>
2025-09-23 21:04:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// Import the new HomePage component
+
 import HomePage from '../components/HomePage.vue';
 import ProductList from '../components/ProductList.vue';
 import ImageGallery from '../components/ImageGallery.vue';
@@ -10,7 +10,7 @@ import ContactForm from '../components/ContactForm.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  // The home route now points to the new HomePage component
+
   { path: '/', component: HomePage }, 
   { path: '/productos', component: ProductList },
   { path: '/galeria', component: ImageGallery },
@@ -22,5 +22,6 @@ const router = new VueRouter({
   mode: 'history',
   routes,
 });
+
 
 export default router;
